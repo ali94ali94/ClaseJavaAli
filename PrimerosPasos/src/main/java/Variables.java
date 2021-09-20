b63898558daaedd1268047268c94452227c949db
Ali Mosachasib <ali94ali94@gmail.com>
2021-09-20 16:56:34 +0200

    primera prueba

---
+++ PrimerosPasos/src/main/java/Variables.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SrAli
+ */
+public class Variables {
+    
+    public static void main(String args[]){
+        
+        short edad=55;
+                
+        System.out.println(edad);
+                
+        
+    }
+    
+}
